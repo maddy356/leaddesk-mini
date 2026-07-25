21ff26cbaff0c5dd1f4c4333384c19306cb3509b
maddy356 <152002449+maddy356@users.noreply.github.com>
2026-07-25 21:34:27 +0530

    Security: Remove files containing MongoDB credentials

--- test-dns.js
+++
@@ -1,6 +0,0 @@
-const dns = require('dns');
-dns.setServers(['8.8.8.8']);
-dns.resolveTxt('cluster0.yjtdoc2.mongodb.net', (err, txt) => {
-  if (err) console.error(err);
-  else console.log(txt);
-});
